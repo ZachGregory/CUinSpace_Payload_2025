95ede81de36e469850fdd843415137987bfe21ee
LucasBramesfeld <LucasRalfB@gmail.com>
2025-03-20 20:27:19 -0400

    Updated temperature_control - weighted average temperature

--- Payload/Software/temperature_control/temperature_control.ino
+++ Payload/Software/temperature_control/temperature_control.ino
@@ -1,68 +1,70 @@
 #include <math.h>
 
-const int ThermistorPin = A0; // Analog pin for thermistor
-const float R1 = 10000.0; // Resistor resistance (ohms)
-
 const int RPWM = 6; // Forward digital pin
 const int LPWM = 5; // Reversal digital pin
 
-float temp; // Temperature in Celsius
-float threshold; // Temperature goal
-float tolerance; // Acceptable range + or -
+const int thermistor[] = {A0, A1, A2}; // Analog pins for thermistors
+const float R1 = 10000.0; // Resistor resistance (ohms) ??? 
+const int numOfThermistors = sizeof(thermistor) / sizeof(thermistor[0]);
+
+float thermistorWeight[] = {1, 1, 1}; // Weighting of each thermistor when calculating environment temp
+bool thermistorIsValid[numOfThermistors];
+
+float temp[numOfThermistors]; // Temperature in Celsius
+float environmentTemp; // Temperature of the environment
+float setpoint = 23.0; // Temperature goal
+float tolerance = 1.0; // Acceptable range +/-
+float errorTolerance = 8.0; // Acceptable difference from the median temp
 
 void setup() {
-  threshold = 30; // Temperature goal
-  tolerance = 1; // Acceptable range + or -
-
   pinMode(RPWM, OUTPUT);
   pinMode(LPWM, OUTPUT);
-  
   Serial.begin(9600);
+  Serial.println("Start:");
 }
 
 void loop() {
-  temp = getTemp();
-
-  if (temp > threshold + tolerance) { // Cooling
-    digitalWrite(RPWM, HIGH);
-    digitalWrite(LPWM, LOW);
-    Serial.print("Cooling");
-  }
-  else if (temp < threshold - tolerance) { // Heating
-    digitalWrite(RPWM, LOW);
-    digitalWrite(LPWM, HIGH);
-    Serial.print("Heating");
-  }
-  else { // Idle
-    digitalWrite(RPWM, LOW);
-    digitalWrite(LPWM, LOW);
-    Serial.print("Idle");
+  for (int i = 0; i < numOfThermistors; i++) { // Get indivudal temp readings
+    temp[i] = getTemp(thermistor[i]);
+    // Plot/Print temp data
+    Serial.print("Thermistor_"); 
+    Serial.print(thermistor[i]); 
+    Serial.print(":"); 
+    Serial.print(temp[i]);
+    Serial.print("\t");
   }
 
-  Serial.print("\t| Temperature:\t");
-  Serial.print(temp);
-  Serial.print(" C");
-  Serial.print("\t| Goal:\t");
-  Serial.print(threshold);
-  Serial.println(" C");
+  detectFaultySensors();
+
+  // Find the weighted average temp
+  environmentTemp = 0;
+  float totalWeight = 0;
+  for (int i = 0; i < numOfThermistors; i++) {
+    if (thermistorIsValid[i]) {
+      environmentTemp += temp[i] * thermistorWeight[i];
+      totalWeight += thermistorWeight[i];
+    }
+  }
+  environmentTemp /= totalWeight;
+
+  Serial.print("Enviroment:"); 
+  Serial.println(environmentTemp);
+
+  controlPeltier(environmentTemp); // Adjust Peltier direction based on temp
 
   delay(500);
 
-
-  if (Serial.available() > 0) {  
-    float newThreshold = Serial.parseFloat();
+  if (Serial.available() > 0) { // Lets the change of the set point from the serial monitor
+    float newSetpoint = Serial.parseFloat();
     while (Serial.available()) Serial.read(); // Clear buffer
-    if (newThreshold != 0) {
-        threshold = newThreshold;
-        Serial.print("Updated Threshold: ");
-        Serial.println(threshold);
-    }
+    setpoint = newSetpoint;
+    Serial.print("Updated Threshold: ");
+    Serial.println(setpoint);
   }
-
 }
 
-float getTemp() {
-  int Vo = analogRead(ThermistorPin);
+float getTemp(int thermistorPin) {
+  int Vo = analogRead(thermistorPin);
   if (Vo == 0) return -273.15; // Avoid division by zero error
 
   float logR2, R2, T;
@@ -70,8 +72,61 @@ float getTemp() {
 
   R2 = R1 * (1023.0 / Vo - 1.0);
   logR2 = log(R2);
-  T = (1.0 / (c1 + c2 * logR2 + c3 * logR2 * logR2 * logR2));
-  T = T - 273.15; // Convert from Kelvin to Celsius
+  T = (1.0 / (c1 + c2 * logR2 + c3 * logR2 * logR2 * logR2)) - 273.15; // Convert from Kelvin to Celsius
 
   return T;
+}
+
+// Find the median and compares all the values to it.
+// If outside the given range mark it as a faulty sensor
+void detectFaultySensors() {
+  float sorted[numOfThermistors];
+
+  // Copy array
+  for (int i = 0; i< numOfThermistors; i++){
+    sorted[i] = temp[i];
+  }
+
+  // Sort to find median
+  int minIndex;
+  for (int i = 0; i< numOfThermistors; i++){
+    minIndex = i;
+    for (int j = i+1; j< numOfThermistors; j++){
+      if (sorted[minIndex] > sorted[j]){
+        minIndex = j;
+      }
+    }
+    float tem = sorted[i];
+    sorted[i] = sorted[minIndex];
+    sorted[minIndex] = tem;
+  }
+
+  float median = sorted[numOfThermistors/2];
+
+  for (int i = 0; i< numOfThermistors; i++){
+    if(abs(temp[i] - median) > errorTolerance){ // Adjust accordingly
+      thermistorIsValid[i] = false; // Mark as faulty
+      Serial.print("Faulty Sensor Detected: Thermistor ");
+      Serial.print(i);
+      Serial.print(" | Reading: ");
+      Serial.print(temp[i]);
+      Serial.print(" C | Median: ");
+      Serial.println(median);
+    } else{
+      thermistorIsValid[i] = true; // Mark as valid
+    }
+  }
+}
+
+void controlPeltier(float temperature) {
+  if (temperature > setpoint + tolerance) { // Cooling
+    digitalWrite(RPWM, HIGH);
+    digitalWrite(LPWM, LOW);
+  } else if (temperature < setpoint - tolerance) { // Heating
+    digitalWrite(RPWM, LOW);
+    digitalWrite(LPWM, HIGH);
+  } else { // Idle
+    digitalWrite(RPWM, LOW);
+    digitalWrite(LPWM, LOW);
+  }
 }
